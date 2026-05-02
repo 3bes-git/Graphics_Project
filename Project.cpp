b486a41c9920f5259537b308d507e886e0d27b8c
3bes <eng.ahmed.fcai@gmail.com>
2026-05-02 23:03:18 +0300

    Refactor code structure and improve readability

--- Project.cpp
+++ Project.cpp
@@ -3,10 +3,11 @@
 
 bool isNight = false;
 float angle = 0.0f;
-float cloudX = -400; // بداية من الشمال
+float cloudX = -400;
 
-void Background() {
-    glClearColor(0.5f, 0.8f, 1.0f, 1.0f); //Day
+void Background()
+{
+    glClearColor(0.5f, 0.8f, 1.0f, 1.0f);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(-450, 450, -300, 300); //900*600
@@ -14,23 +15,27 @@ void Background() {
 
 void keyboard(unsigned char key, int x, int y)
 {
-    if (key == 'n' || key == 'N') {
+    if (key == 'n' || key == 'N')
+    {
         isNight = true;
-        glClearColor(0.0f, 0.0f, 0.2f, 1.0f); //Night
+        glClearColor(0.0f, 0.0f, 0.2f, 1.0f);
     }
-    if (key == 'l' || key == 'L') {
+    if (key == 'l' || key == 'L')
+    {
         isNight = false;
-        glClearColor(0.5f, 0.8f, 1.0f, 1.0f); //Day
+        glClearColor(0.5f, 0.8f, 1.0f, 1.0f);
     }
     if (key == 27) exit(0);
 
     glutPostRedisplay();
 }
 
-void drawCircle(float cx, float cy, float radius, float r, float g, float b) {
-    glColor3f(r, g, b);
+void drawCircle(float cx, float cy, float radius, float r, float g, float b)
+{
+    glColor3f(r, g, b);     
     glBegin(GL_POLYGON);
-    for (int i = 0; i < 360; i++) {
+    for (int i = 0; i < 360; i++)
+    {
         float theta = i * 3.14159f / 180.0f;
         float x = cx + radius * cos(theta);
         float y = cy + radius * sin(theta);
@@ -38,15 +43,18 @@ void drawCircle(float cx, float cy, float radius, float r, float g, float b) {
     }
     glEnd();
 }
-void drawCloud(float x, float y) {
+void drawCloud(float x, float y)
+{
     drawCircle(x, y, 20, 1, 1, 1);
     drawCircle(x+20, y+10, 25, 1, 1, 1);
     drawCircle(x+40, y, 20, 1, 1, 1);
 }
-void update() {
+void update()
+{
     cloudX += 0.06; // سرعة الحركة
 
-    if (cloudX > 450) {
+    if (cloudX > 450)
+    {
         cloudX = -450; // ترجع من الأول
     }
 
@@ -74,7 +82,8 @@ void drawWindmill(float x, float y) {
     }
     glPopMatrix();
 }
-void timer(int v) {
+void timer(int v)
+{
     angle += 2.0f;
     glutPostRedisplay();
     glutTimerFunc(16, timer, 0);
@@ -85,48 +94,51 @@ void drawHouse(float x, float y)
     //Main Square
     glColor3f(0.96f, 0.87f, 0.70f);
     glBegin(GL_QUADS);
-        glVertex2f(x-60, y);
-        glVertex2f(x+60, y);
-        glVertex2f(x+60, y+80);
-        glVertex2f(x-60, y+80);
+    glVertex2f(x-60, y);
+    glVertex2f(x+60, y);
+    glVertex2f(x+60, y+80);
+    glVertex2f(x-60, y+80);
     glEnd();
 
     //Triangle
     glColor3f(0.4f, 0.0f, 0.0f);
     glBegin(GL_TRIANGLES);
-        glVertex2f(x-80, y+80);
-        glVertex2f(x+80, y+80);
-        glVertex2f(x, y+140);
+    glVertex2f(x-80, y+80);
+    glVertex2f(x+80, y+80);
+    glVertex2f(x, y+140);
     glEnd();
 
     //Door
     glColor3f(0.25f, 0.13f, 0.05f);
     glBegin(GL_QUADS);
-        glVertex2f(x-15, y);
-        glVertex2f(x+15, y);
-        glVertex2f(x+15, y+50);
-        glVertex2f(x-15, y+50);
+    glVertex2f(x-15, y);
+    glVertex2f(x+15, y);
+    glVertex2f(x+15, y+50);
+    glVertex2f(x-15, y+50);
     glEnd();
-    if (isNight) {
+    if (isNight)
+    {
         glColor3f(1.0f, 1.0f, 0.0f);
-    } else {
+    }
+    else
+    {
         glColor3f(0.7f, 0.9f, 1.0f);
     }
 
     //L window
     glBegin(GL_QUADS);
-        glVertex2f(x-45, y+40);
-        glVertex2f(x-20, y+40);
-        glVertex2f(x-20, y+65);
-        glVertex2f(x-45, y+65);
+    glVertex2f(x-45, y+40);
+    glVertex2f(x-20, y+40);
+    glVertex2f(x-20, y+65);
+    glVertex2f(x-45, y+65);
     glEnd();
 
     //R window
     glBegin(GL_QUADS);
-        glVertex2f(x+20, y+40);
-        glVertex2f(x+45, y+40);
-        glVertex2f(x+45, y+65);
-        glVertex2f(x+20, y+65);
+    glVertex2f(x+20, y+40);
+    glVertex2f(x+45, y+40);
+    glVertex2f(x+45, y+65);
+    glVertex2f(x+20, y+65);
     glEnd();
 }
 
@@ -137,44 +149,99 @@ void Draw()
     glClear(GL_COLOR_BUFFER_BIT);
 
 
-    if (!isNight) {
+    if (!isNight)
+    {
         drawCircle(300, 200, 50, 1.0f, 1.0f, 0.0f);
         drawCloud(cloudX, 200);
         drawCloud(cloudX + 200, 230);
-    } else { //moon
+    }
+    else     //moon
+    {
         drawCircle(300, 200, 50, 0.9f, 0.9f, 0.9f);
         drawCircle(320, 200, 50, 0.0f, 0.0f, 0.2f);
     }
 
     //Ground
-      glColor3f(0, 0.6, 0);
+    glColor3f(0, 0.6, 0);
     glBegin(GL_QUADS);
-        glVertex2f( -450, -1050);
-        glVertex2f(450, -1050);
-        glVertex2f(450, -150);
-        glVertex2f(-450, -150);
+    glVertex2f( -450, -1050);
+    glVertex2f(450, -1050);
+    glVertex2f(450, -150);
+    glVertex2f(-450, -150);
     glEnd();
 
     //Houses
-   drawHouse(-350, -200);
-drawHouse(340, -200);
-    drawWindmill(0, -200);
+    drawHouse(-350, -200);
+    drawHouse(340, -200);
+    drawWindmill(150, -200);
 
-   // Tree
-glColor3f(0.4f, 0.2f, 0.0f);
-glBegin(GL_QUADS);
+    // Tree
+    glColor3f(0.4f, 0.2f, 0.0f);
+    glBegin(GL_QUADS);
     glVertex2f(-220, -40);
     glVertex2f(-200, -40);
     glVertex2f(-200, -200);
     glVertex2f(-220, -200);
-glEnd();
+    glEnd();
 
-drawCircle(-210, -50, 35, 0, 0.6, 0);
-drawCircle(-210, -20, 30, 0, 0.7, 0);
-drawCircle(-180, -50, 30, 0, 0.6, 0);
-drawCircle(-240, -50, 30, 0, 0.6, 0);
-drawCircle(-210, -80, 25, 0, 0.5, 0);
-        glFlush();
+    drawCircle(-210, -50, 35, 0, 0.6, 0);
+    drawCircle(-210, -20, 30, 0, 0.7, 0);
+    drawCircle(-180, -50, 30, 0, 0.6, 0);
+    drawCircle(-240, -50, 30, 0, 0.6, 0);
+    drawCircle(-210, -80, 25, 0, 0.5, 0);
+
+// Windmill
+//    glPushMatrix();
+//    glTranslatef(290, 0, 0);
+//
+//
+//    glColor3f(0.0f, 0.0f, 0.0f);
+//    glBegin(GL_QUADS);
+//    glVertex2f(-269, 1);
+//    glVertex2f(-351, 1);
+//    glVertex2f(-351, -199);
+//    glVertex2f(-269, -199);
+//    glEnd();
+//    glColor3f(1.0f, 1.0f, 1.0f);
+//    glBegin(GL_QUADS);
+//    glVertex2f(-270, 0);
+//    glVertex2f(-350, 0);
+//    glVertex2f(-350, -200);
+//    glVertex2f(-270, -200);
+//    glEnd();
+//    glColor3f(1.0f, 0.0f, 0.0f);
+//    glBegin(GL_TRIANGLES);
+//    glVertex2f(-360, 0);
+//    glVertex2f(-260, 0);
+//    glVertex2f(-310, 60);
+//    glEnd();
+//
+//    glColor3f(0.25f, 0.13f, 0.05f);
+//    glBegin(GL_QUADS);
+//    glVertex2f(-285, -200);
+//    glVertex2f(-330, -200);
+//    glVertex2f(-330, -140);
+//    glVertex2f(-285, -140);
+//    glEnd();
+//
+//    glPushMatrix();
+//    glTranslatef(-310, 20, 0);
+//    glRotatef(angle, 0, 0, 1);
+//    glColor3f(0.8f, 0.8f, 0.8f);
+//    for (int i = 0; i < 4; i++)
+//    {
+//        glRotatef(90, 0, 0, 1);
+//        glBegin(GL_QUADS);
+//        glVertex2f(-5, 0);
+//        glVertex2f(5, 0);
+//        glVertex2f(5, 100);
+//        glVertex2f(-5, 100);
+//        glEnd();
+//    }
+//    glPopMatrix();
+//    glPopMatrix();
+
+    glFlush();
 
 }
 
